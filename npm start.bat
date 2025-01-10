de047877cac69de1c1b301de04fb8c60c1b528b9
_rutujaaa_14 <lubalrutuja14@gmail.com>
2025-01-10 09:52:35 +0530

    Add files via upload

---
+++ npm start.bat
@@ -0,0 +1,4 @@
+start cmd /k "cd /d C:\Users\ZEN\Desktop\PCOS - CYCLE SENSE\LOGIN\ && npm start" 
+start cmd /k "cd /d C:\Users\ZEN\Desktop\PCOS - CYCLE SENSE\NODE-BACKEND\ && nodemon app"
+start cmd /k "cd /d C:\Users\ZEN\Desktop\PCOS - CYCLE SENSE\period-calculator\ && set PORT=3001 && npm start"
+start cmd /k "cd /d C:\Users\ZEN\Desktop\PCOS - CYCLE SENSE\sans\project\ && set PORT=3002 && npm start"
